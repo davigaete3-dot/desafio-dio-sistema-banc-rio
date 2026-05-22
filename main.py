38d72719c9ab54dd78eae171af21e038da532da2
davigaete3-dot <davigaete3@gmail.com>
2026-05-22 12:59:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 conta = [100]
 depositos_feitos = []
 saques_feitos= []
+conta_new = {}
 
+def criar_nova_conta():
+    nome = input('Digite seu nome: ')
+    idade = int(input('Digite sua idade: '))
+    gmail = input('Digite seu Gmail: ')
+    if '@' in gmail:
+        conta_new[nome] = {
+            
+            'idade': idade,
+            'gmail': gmail
+        }   
+        print('Nova conta criada')
+        print(conta_new)
+    else:
+        print('gmail invalido tente novamente')
+
+def listar_contas_criadas():
+    for nome in conta_new:
+        print(f'conta criadas para o usuário {nome}')
 def deposito():
     valor = float (input('qual valor deseja depositar?: '))
     conta[0] += valor
@@ -25,7 +44,9 @@ while True:
     print('1-depositar')
     print('2-sacar')
     print('3-extrato')
-    print('4-sair')
+    print('4-cadastrar conta')
+    print('5-lista de contas')
+    print('6-sair')
     opcao=int(input('escolha uma das opções a cima: '))
     if opcao==1:
         deposito()
@@ -33,8 +54,13 @@ while True:
         saque()
     elif opcao==3:
         extrato()
+    elif opcao==4:
+        criar_nova_conta()
+    elif opcao==5:
+        listar_contas_criadas()
     else: 
         break
+
 
 
 
